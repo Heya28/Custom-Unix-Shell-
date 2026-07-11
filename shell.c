2e4b01aaa7a6180299c0af2a48e9af3d353dfd9a
hiyat <hiyatrehan@gmail.com>
2026-07-11 20:58:41 +0000

    Debug: tokenize() in shell.c 1.free(token) 2. multiple spaces problem 3. tiny optimization int len instead of strlen() in for loop

--- shell.c
+++ shell.c
@@ -8,6 +8,7 @@
 #define MAX_TOKEN_SIZE 64
 #define MAX_NUM_TOKENS 64
 
+// spilt the input string by space and return the array of strings. 
 char** tokenize(char* inputLine){
     int tokenIndex=0;
     int tokenNo=0;
@@ -17,16 +18,19 @@ char** tokenize(char* inputLine){
 
     for(int i=0;i<strlen(inputLine);i++){
         if(inputLine[i]==' '|| inputLine[i]=='\t' || inputLine[i]=='\n' ){
+            if(tokenIndex!=0){
             token[tokenIndex]='\0'; // end of the token append. 
             alltokens[tokenNo]=malloc(MAX_TOKEN_SIZE*sizeof(char));
             strcpy(alltokens[tokenNo],token); // copy token char sequence ending with \0 to alltokens[tokenNo]
             tokenNo++;
             tokenIndex=0; // next word starts writing from index 0, essentially overwriting the previous. 
+            }
         }else{
             token[tokenIndex]=inputLine[i];
             tokenIndex++;
         }
     }
     alltokens[tokenNo]=NULL; // required for execvp(), otherwise it keeps reading garbage.
+    free(token); // temporary memory not required after loop ends. 
     return alltokens;
 }
