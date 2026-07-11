6e984af59ede471b7fa4ff8bba2135189190fd07
hiyat <hiyatrehan@gmail.com>
2026-07-11 21:53:25 +0000

    Added: int main() , Debug: Edge case for no separator at the end of inputLine in tokenize()

--- shell.c
+++ shell.c
@@ -12,11 +12,11 @@
 char** tokenize(char* inputLine){
     int tokenIndex=0;
     int tokenNo=0;
-    int i;
+    int len=strlen(inputLine);
     char **alltokens = malloc(MAX_NUM_TOKENS * sizeof(char *));
     char *token=malloc(MAX_TOKEN_SIZE * sizeof(char));
 
-    for(int i=0;i<strlen(inputLine);i++){
+    for(int i=0;i<len;i++){
         if(inputLine[i]==' '|| inputLine[i]=='\t' || inputLine[i]=='\n' ){
             if(tokenIndex!=0){
             token[tokenIndex]='\0'; // end of the token append. 
@@ -30,7 +30,35 @@ char** tokenize(char* inputLine){
             tokenIndex++;
         }
     }
+    if(tokenIndex!=0){
+        token[tokenIndex]='\0';  // incase of no separator at the end of inputline (edge case eg. ls)
+        // allocate memory
+        alltokens[tokenNo]=malloc(MAX_TOKEN_SIZE*sizeof(char));
+        strcpy(alltokens[tokenNo],token);
+        tokenNo++;
+    }  
     alltokens[tokenNo]=NULL; // required for execvp(), otherwise it keeps reading garbage.
     free(token); // temporary memory not required after loop ends. 
     return alltokens;
+}
+
+int main(int argc, char* argv[]){
+    char inputLine[1024];
+    char **alltokens;
+    while(1){
+        // clear out old input
+        memset(inputLine,0,sizeof(inputLine));
+        // take input
+        printf("$ ");
+        scanf("%[^\n]");
+        getchar(); // get rid of \n character before next iteration
+
+        alltokens=tokenize(inputLine);
+        // free alltokens memory
+        for(int k=0; alltokens[k]!=NULL;k++){
+            free(alltokens[k]);
+        }
+        free(alltokens);
+    }
+    return 0;
 }
